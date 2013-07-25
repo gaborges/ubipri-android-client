32f2bcc863e0ab9a902c1a656eb736755459a424
Guilhermeborges.PF <Guilhermeborges.PF@73d080d6-7368-4d6d-f900-43fc4b906c31>
2013-07-25 02:07:34 +0000

    Adição do método de login

---
+++ trunk/AndroidClient/src/br/ufrgs/inf/ubipri/client/communication/TaskRemoteLogin.java
@@ -0,0 +1,71 @@
+package br.ufrgs.inf.ubipri.client.communication;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import br.ufrgs.inf.ubipri.util.Config;
+
+import android.os.AsyncTask;
+
+public class TaskRemoteLogin extends AsyncTask<Object, Void, String> {
+	
+	private String result;
+
+	@Override
+	protected void onPostExecute(String result) {
+		// TODO Auto-generated method stub
+		super.onPostExecute(result);
+	}
+
+	@Override
+	protected void onPreExecute() {
+		// TODO Auto-generated method stub
+		super.onPreExecute();
+		result = "";
+	}
+
+	@Override
+	protected String doInBackground(Object ... params) {
+		try {
+	        URL url = new URL(Config.SERVER_HOST+"webresources/rest/validate/"+params[0]+"/"+params[1]+"/"+params[2]);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	                conn.setDoOutput(true);
+	                conn.setRequestMethod("GET");
+	                conn.setRequestProperty("Accept", "application/json");
+	                conn.setRequestProperty("Content-Type", "application/json");
+	                conn.connect();
+	 
+			if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+				throw new RuntimeException("Failed : HTTP error code : "
+					+ conn.getResponseCode());
+			} 
+	 
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					(conn.getInputStream())));
+	 
+			String output;
+			while ((output = br.readLine()) != null) {
+				result += output;
+			}
+	 
+			conn.disconnect();
+	        } catch (MalformedURLException e) {
+	 
+			e.printStackTrace();
+	 
+		  } catch (IOException e) {
+	 
+			e.printStackTrace();
+	 
+		  }
+		return result;
+	}
+
+	
+
+}
